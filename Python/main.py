4e539300e6f6773eed23fe49415506fb14cce6b7
timmy0402 <minhnhathb04@gmail.com>
2025-06-07 12:50:45 -0700

    reorganized files

--- Python/main.py
+++ Python/main.py
@@ -12,20 +12,24 @@ import queue
 from collections import deque
 from scipy.spatial.transform import Rotation as R
 
-
-SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9C"
-RX_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9C"
-TX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9C"
+# defining Arduino UUID
+SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9B"
+RX_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9B"
+TX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9B"
 
 app = dash.Dash(__name__)
 
-data_queue = queue.Queue()  # Queue for data transfer
+# initialized queue with length of 10
+# older values will the thrown away
+data_queue = deque(maxlen=10)
+queue_lock = threading.Lock()
 
-axis_limit = 30
+axis_limit = 50  # axis limit for graphin
 
-curr_rotation = np.eye(3)
-curr_time = None
+curr_rotation = np.eye(3)  # 3x3 identity matrix
+curr_time = None  # track time of readings
 
+# app layout
 app.layout = html.Div(
     children=[
         dcc.Graph(
@@ -53,7 +57,7 @@ app.layout = html.Div(
             ),
         ),
         dcc.Interval(
-            id="interval-component", interval=300, n_intervals=0  # in milliseconds
+            id="interval-component", interval=100, n_intervals=0  # in milliseconds
         ),
         html.Button("Start/Stop", id="submit-val", n_clicks=0),
     ]
@@ -61,7 +65,12 @@ app.layout = html.Div(
 
 
 async def run_ble(data_queue: queue.Queue):
+    """
+    This function start connection to the UNO_R4_UART
+    :param data_queue: A queue for holding data from the UNO R4
+    """
     while True:
+        # finding the UNO R4
         print("Scanning for UNO_R4_UART...")
         device = await BleakScanner.find_device_by_filter(
             lambda d, ad: d.name and "UNO_R4_UART" in d.name
@@ -71,38 +80,37 @@ async def run_ble(data_queue: queue.Queue):
             await asyncio.sleep(3)
             continue
 
-        # try:
-        async with BleakClient(device) as client:
-            print("Connected to", device.address)
+        try:
+            async with BleakClient(device) as client:
+                # device connected
+                print("Connected to", device.address)
 
-            def on_rx(_, data):
-                try:
-                    json_data = json.loads(data.decode().strip())
-                    # print(json_data)
-                    # time = json_data["time"]
-                    new_data = {
-                        "a_x": json_data["accel"]["x"],
-                        "a_y": json_data["accel"]["y"],
-                        "a_z": json_data["accel"]["z"],
-                        "g_x": json_data["gyro"]["x"],
-                        "g_y": json_data["gyro"]["y"],
-                        "g_z": json_data["gyro"]["z"],
-                        "time": json_data["time"],
-                    }
+                # on data send to R_X port
+                def on_rx(_, data):
+                    try:
+                        json_data = json.loads(data.decode().strip())
+                        new_data = {
+                            "a_x": json_data["accel"]["x"],
+                            "a_y": json_data["accel"]["y"],
+                            "a_z": json_data["accel"]["z"],
+                            "g_x": json_data["gyro"]["x"],
+                            "g_y": json_data["gyro"]["y"],
+                            "g_z": json_data["gyro"]["z"],
+                            "time": json_data["time"],
+                        }
 
-                    data_queue.put(new_data)
-                # data.append(json_data)
-                # ("From Arduino:", data.decode().strip())
+                        with queue_lock:
+                            data_queue.append(new_data)
+                    except json.JSONDecodeError:
+                        print("Failed to decode JSON")
 
-                except json.JSONDecodeError:
-                    print("Failed to decode JSON")
+                await client.start_notify(TX_UUID, on_rx)
+                while True:
+                    await asyncio.sleep(1)
+        except Exception as e:
+            print("BLE connection error:", e)
 
-            await client.start_notify(TX_UUID, on_rx)
-            while True:
-                await asyncio.sleep(1)
-        # except Exception as e:
-        # print("BLE connection error:", e)
-        # await asyncio.sleep(3)
+        await asyncio.sleep(3)
 
 
 def start_ble(data_queue: queue.Queue):
@@ -123,12 +131,16 @@ def start_ble(data_queue: queue.Queue):
     State("live-3d-graph", "figure"),
 )
 def update_graph(n, current_fig):
+
     new_data = None
-    try:
-        new_data = data_queue.get_nowait()
-    except queue.Empty:
-        print("No data in queue")
-        pass
+    with queue_lock:
+        if data_queue:
+            new_data = data_queue[-1]  # get the latest data
+            data_queue.clear()  # clear all older messages to avoid lag
+        else:
+            print("No data in deque")
+            return dash.no_update
+
     # calculation
     if new_data:
 
@@ -137,39 +149,61 @@ def update_graph(n, current_fig):
         x = list(curr_data["x"])
         y = list(curr_data["y"])
         z = list(curr_data["z"])
-        pos = np.array([x[-1], y[-1], z[-1]])
+        pos = np.array([x[-1], y[-1], z[-1]])  # get the last positiob
 
         global curr_time, curr_rotation
-        curr_vel = np.array([0, 0, 0])
+        curr_vel = np.array([0, 0, 0])  # reset velocity to 0
         if curr_time == None:
-            curr_time = new_data["time"]
+            curr_time = new_data["time"]  # this is to match the time of the data sent
 
         dt = (new_data["time"] - curr_time) / 1000
 
-        curr_time = new_data["time"]
+        curr_time = new_data["time"]  # update time
 
-        omega = np.array([new_data["g_x"], new_data["g_y"], new_data["g_z"]])
-        accel = np.array([new_data["a_x"], new_data["a_y"], new_data["a_z"] - 9.81])
+        omega = np.array(
+            [new_data["g_x"], new_data["g_y"], new_data["g_z"]]
+        )  # rotation vec
+        # round down
+        a_x = 0
+        a_y = 0
+        if abs(0 - new_data["a_x"]) < 0.2:
+            a_x = 0
+        else:
+            a_x = new_data["a_x"]
+        if abs(0 - new_data["a_y"]) < 0.7:
+            a_y = 0
+        else:
+            a_y = new_data["a_y"]
+        accel = np.array([a_x, a_y, new_data["a_z"] - 9.81])  # acceleration vect
+        print(accel)
+        d_theta = omega * dt  # new angles
+        d_rotation = R.from_rotvec(
+            d_theta
+        ).as_matrix()  # rotation matrix of those angle
 
-        d_theta = omega * dt
-        d_rotation = R.from_rotvec(d_theta).as_matrix()
+        new_rotation = np.matmul(
+            curr_rotation, d_rotation
+        )  # this would add up the changes
 
-        new_rotation = np.matmul(curr_rotation, d_rotation)
+        # curr_rotation = new_rotation
 
         new_accel = np.matmul(new_rotation, accel)
+        magnitude = np.linalg.norm(new_accel)
+        if magnitude >= 1.0:
 
-        curr_vel = curr_vel + new_accel * dt
-        new_pos = pos + curr_vel * dt
+            curr_vel = curr_vel + new_accel * dt
+            new_pos = pos + curr_vel * dt
 
-        print(
-            f"omega: {omega}, accel: {accel}, dt: {dt}, d_theta: {d_theta}, d_rotation: {d_rotation}"
-        )
-        print(
-            f"new_rotation: {new_rotation}, new_accel: {new_accel}, curr_vel={curr_vel}, pos: {new_pos}"
-        )
-        x.append(new_pos[0] if new_pos[0] <= axis_limit else axis_limit)
-        y.append(new_pos[1] if new_pos[1] <= axis_limit else axis_limit)
-        z.append(new_pos[2] if new_pos[2] <= axis_limit else axis_limit)
+            new_pos = np.clip(new_pos, -axis_limit, axis_limit)
+
+            # print(
+            #    f"new_rotation: {new_rotation}, new_accel: {new_accel}, curr_vel={curr_vel}, pos: {new_pos}"
+            # )
+            x.append(new_pos[0])
+            y.append(new_pos[1])
+            z.append(0)
+        else:
+            return dash.no_update
 
         fig = go.Figure(
             data=[
